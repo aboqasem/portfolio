f5bd32b85354f4423479ca7ff4a8f35c932122b7
Mohammad Al Zouabi <mb.alzouabi@gmail.com>
2021-02-09 10:03:24 +0800

    Add particlesjs

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import "./styles.css";
+import Particles from "particlesjs";
 
 ReactDOM.render(
   <React.StrictMode>
@@ -8,3 +10,35 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById("root")
 );
+
+window.onload = () => {
+  Particles.init({
+    selector: ".background",
+    maxParticles: 300,
+    sizeVariations: 5,
+    color: ["#2c7744", "#5a3f37"],
+    responsive: [
+      {
+        breakpoint: 840,
+        options: {
+          maxParticles: 200,
+          sizeVariations: 4,
+        },
+      },
+      {
+        breakpoint: 600,
+        options: {
+          maxParticles: 150,
+          sizeVariations: 4,
+        },
+      },
+      {
+        breakpoint: 480,
+        options: {
+          maxParticles: 80,
+          sizeVariations: 3,
+        },
+      },
+    ],
+  });
+};

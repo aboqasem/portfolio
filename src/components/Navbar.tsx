19078b5de21e8a1683b51baa4b1aad096416e7ed
Mohammad Al Zouabi <mb.alzouabi@gmail.com>
2021-02-10 23:50:27 +0800

    Fix mistake in replacing text

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { AppBar, Slide, Toolbar, Typography, useScrollTrigger } from '@material-ui/core';
 
-> {
+const Navbar = (): JSX.Element => {
   const trigger = useScrollTrigger();
   return (
     <Slide in={!trigger}>

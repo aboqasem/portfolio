9ff9d91840eb2ad7d0eb93a88711fdcb2c98cf60
Mohammad Al Zouabi <62098043+aboqasem@users.noreply.github.com>
2021-02-12 10:23:15 +0800

    Change hero sentence

--- src/pages/Home/Hero.tsx
+++ src/pages/Home/Hero.tsx
@@ -7,7 +7,7 @@ const Hero = (): JSX.Element => {
   return (
     <HomeSection hero theme={tDarkTheme} title={"Hey, I'm Mohammad Al Zouabi."}>
       <Typography variant={'body1'} style={{ position: 'relative', zIndex: 1 }}>
-        Enthusiastic software developer who stands for quality, consistency, and attention to details.
+        An enthusiastic software developer that stands for quality, consistency, and attention to details.
       </Typography>
 
       {/* Responsive sized box */}

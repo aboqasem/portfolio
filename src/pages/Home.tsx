1c257d74eb12d701d66b788bd431a4f039f4a2fc
Mohammad Al Zouabi <62098043+aboqasem@users.noreply.github.com>
2021-02-21 03:06:12 +0800

    Basic Home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,9 +3,11 @@ import React from 'react';
 const Home = (): JSX.Element => {
   return (
     <>
-      <p className="text-2xl">Hey, I'm Mohammad Al Zouabi.</p>
-      <p className="text-xl">
-        An enthusiastic software developer that stands for quality, consistency, and attention to details.
+      <p className="text-xl  mb-4 sm:text-2xl md:text-3xl" style={{ color: '#664D33' }}>
+        Hey, I'm Mohammad Al Zouabi.
+      </p>
+      <p className="text-lg sm:text-xl md:text-2xl">
+        An enthusiastic software developer who stands for quality, consistency, and attention to details.
       </p>
     </>
   );

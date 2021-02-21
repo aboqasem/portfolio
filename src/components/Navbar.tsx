b98cb373522e16874bf655886e7a7e25f3de7aad
Mohammad Al Zouabi <62098043+aboqasem@users.noreply.github.com>
2021-02-22 04:18:44 +0800

    Blur Navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,7 +5,10 @@ const Navbar = (): JSX.Element => {
   const { pathname } = useLocation();
 
   return (
-    <header className="bg-transparent px-6 py-3 flex flex-wrap md:px-14 md:py-4">
+    <header
+      className="bg-transparent px-6 py-3 flex flex-wrap md:px-14 md:py-4"
+      style={{ backdropFilter: 'blur(8px)' }}
+    >
       <div className="flex-1 flex items-center select-none">
         <Link to="/" className={`text-xl sm:text-2xl md:text-3xl ${pathname === '/' && 'pointer-events-none'}`}>
           aboqasem<p className="animate-bounce inline-block pl-2">🦅</p>

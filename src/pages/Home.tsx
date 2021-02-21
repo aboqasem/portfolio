c95b58d24ea75f9e3c100bec588f6409a328c104
Mohammad Al Zouabi <62098043+aboqasem@users.noreply.github.com>
2021-02-21 18:37:14 +0800

    Convert Home to a class component

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,16 +1,19 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const Home = (): JSX.Element => {
-  return (
-    <>
-      <p className="text-xl  mb-4 sm:text-2xl md:text-3xl" style={{ color: '#664D33' }}>
-        Hey, I'm Mohammad Al Zouabi.
-      </p>
-      <p className="text-lg sm:text-xl md:text-2xl">
-        An enthusiastic software developer who stands for quality, consistency, and attention to details.
-      </p>
-    </>
-  );
-};
+class Home extends Component {
+  // eslint-disable-next-line class-methods-use-this
+  render(): JSX.Element {
+    return (
+      <>
+        <p className="text-xl  mb-4 sm:text-2xl md:text-3xl" style={{ color: '#664D33' }}>
+          Hey, I'm Mohammad Al Zouabi.
+        </p>
+        <p className="text-lg sm:text-xl md:text-2xl">
+          An enthusiastic software developer who stands for quality, consistency, and attention to details.
+        </p>
+      </>
+    );
+  }
+}
 
 export default Home;

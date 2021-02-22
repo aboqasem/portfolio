130b908e557c1f0480ec0a8511e8c4ed39198eef
Mohammad Al Zouabi <mb.alzouabi@gmail.com>
2021-02-23 01:30:42 +0800

    Improve NotFound

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,7 +1,11 @@
 import React from 'react';
 
 const NotFound = (): JSX.Element => {
-  return <p className="text-2xl">Not Found.</p>;
+  return (
+    <>
+      <p className="text-3xl text-center text-red-800 sm:text-5xl md:text-6xl">Page Not Found</p>
+    </>
+  );
 };
 
 export default NotFound;

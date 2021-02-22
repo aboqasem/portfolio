f48791fc131448a2ec22ed6d1b52bd69796bab12
Mohammad Al Zouabi <mb.alzouabi@gmail.com>
2021-02-23 01:43:00 +0800

    Change document title :)

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,6 +1,10 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 const NotFound = (): JSX.Element => {
+  useEffect(() => {
+    document.title = 'Not Found';
+  }, []);
+
   return (
     <>
       <p className="text-3xl text-center text-red-800 sm:text-5xl md:text-6xl">Page Not Found</p>

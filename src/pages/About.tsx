3246e3c72419367e594c050defedabd3810f00b5
Mohammad Al Zouabi <mb.alzouabi@gmail.com>
2021-02-23 03:06:22 +0800

    Add simple projects section to about

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,24 +1,24 @@
-import React from 'react';
-import { myInformation } from '../common/data';
+import React, { useEffect } from 'react';
+import { myProjects } from '../common/data';
 
 const About = (): JSX.Element => {
+  useEffect(() => {
+    document.title = 'About — Mohammad Al Zouabi';
+  }, []);
+
   return (
     <>
-      <p>Software engineering student who is eager to learn. Here is some information about me:</p>
-      <br />
-      <ul>
-        {myInformation.map((v, i) => (
-          <li key={`myInformation@${i}`}>{v.text}</li>
+      <div className="grid gap-2 grid-cols-1 md:grid-cols-2 auto-rows-min">
+        <div className="text-dallas text-3xl self-center text-center mb-5 md:text-4xl">Some Mini Side Projects</div>
+        {myProjects.map((p, i) => (
+          <a href={p.sourceCode || p.url} target="_blank" rel="noreferrer" key={`myProjects@${i}`}>
+            <video className="object-cover w-full h-full rounded-xl shadow-xl" autoPlay loop muted>
+              <source src={p.preview} type="video/webm" />
+              {p.previewMp4 && <source src={p.previewMp4} type="video/mp4" />}
+              Your browser does not support the <code>video</code> element.
+            </video>
+          </a>
         ))}
-      </ul>
-      <div>
-        <a
-          href="https://drive.google.com/file/d/1BC5VROktcMhvsBnkWtApbYVd7gO882o_/view?usp=sharing"
-          target="_blank"
-          rel="noreferrer"
-        >
-          View Resume
-        </a>
       </div>
     </>
   );

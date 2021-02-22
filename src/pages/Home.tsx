4b2b6d46a9d16b1947aba5f6171dd3f33f40368d
Mohammad Al Zouabi <mb.alzouabi@gmail.com>
2021-02-23 01:40:42 +0800

    Convert Home to a functional component

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,23 +2,21 @@ import React, { Component } from 'react';
 import RainStage from '../components/RainStage';
 import { myTechnologies } from '../common/data';
 
-export class Home extends Component {
-  render = (): JSX.Element => {
-    return (
-      <>
-        <RainStage icons={myTechnologies} />
-        <div
-          className="p-5 rounded-xl shadow-md bg-gradient-to-br from-vista-white via-transparent to-vista-white md:shadow-xl sm:p-7 md:p-10"
-          style={{ backdropFilter: 'blur(8px)' }}
-        >
-          <p className="text-dallas text-xl mb-4 sm:text-2xl md:text-3xl">Hey, I'm Mohammad Al Zouabi.</p>
-          <p className="text-lg sm:text-xl md:text-2xl">
-            An enthusiastic software developer who stands for quality, consistency, and attention to details.
-          </p>
-        </div>
-      </>
-    );
-  };
-}
+const Home = (): JSX.Element => {
+  return (
+    <>
+      <RainStage icons={myTechnologies} />
+      <div
+        className="p-5 rounded-xl shadow-md bg-gradient-to-br from-vista-white via-transparent to-vista-white md:shadow-xl sm:p-7 md:p-10"
+        style={{ backdropFilter: 'blur(8px)' }}
+      >
+        <p className="text-dallas text-xl mb-4 sm:text-2xl md:text-3xl">Hey, I'm Mohammad Al Zouabi.</p>
+        <p className="text-lg sm:text-xl md:text-2xl">
+          An enthusiastic software developer who stands for quality, consistency, and attention to details.
+        </p>
+      </div>
+    </>
+  );
+};
 
 export default Home;

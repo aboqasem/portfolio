8d579c8e2a498d72964fbd36c7398acb4c1f8483
Mohammad Al Zouabi <mb.alzouabi@gmail.com>
2021-02-23 10:28:57 +0800

    Add Loading component

---
+++ src/pages/Loading.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Loading = (): JSX.Element => {
+  return (
+    <>
+      <p className="text-3xl text-center animate-pulse sm:text-5xl md:text-6xl">Loading...</p>
+    </>
+  );
+};
+
+export default Loading;

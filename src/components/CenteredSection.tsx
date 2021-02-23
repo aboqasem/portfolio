b097893ac18c0e1296191d0f04745d674fa0490d
Mohammad Al Zouabi <mb.alzouabi@gmail.com>
2021-02-23 15:30:01 +0800

    Better CenteredSection spacing

--- src/components/CenteredSection.tsx
+++ src/components/CenteredSection.tsx
@@ -1,11 +1,16 @@
 import React, { ReactNode } from 'react';
+import { useLocation } from 'react-router-dom';
 
 interface IProps {
   children: ReactNode | ReactNode[];
 }
 
 const CenteredSection = (props: IProps): JSX.Element => {
-  return <div className="max-w-xl mx-auto py-14 px-10 md:px-0">{props.children}</div>;
+  const { pathname } = useLocation();
+
+  return (
+    <div className={`max-w-xl mx-auto ${pathname === '/' ? 'pt-14' : 'pt-5'} pb-8 px-10 md:px-0`}>{props.children}</div>
+  );
 };
 
 export default CenteredSection;

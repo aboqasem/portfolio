e57748b8b26f95dc9a1cffba0b0f6f3cefda4ed9
Mohammad Al Zouabi <mb.alzouabi@gmail.com>
2021-03-01 16:16:04 +0800

    Add hover overlay

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,4 +1,6 @@
 import React, { useEffect } from 'react';
+import { MdOpenInNew } from 'react-icons/md';
+import { BsCodeSlash } from 'react-icons/bs';
 import { myProjects } from '../common/data';
 
 const About = (): JSX.Element => {
@@ -8,16 +10,38 @@ const About = (): JSX.Element => {
 
   return (
     <>
-      <div className="grid gap-2 grid-cols-1 md:grid-cols-2 auto-rows-min">
+      <div className="grid gap-4 grid-cols-1 auto-rows-min md:grid-cols-2 md:gap-2">
         <div className="text-dallas text-3xl self-center text-center mb-5 md:text-4xl">Some Mini Side Projects</div>
         {myProjects.map((p, i) => (
-          <a href={p.sourceCode || p.url} target="_blank" rel="noreferrer" key={`myProjects@${i}`}>
+          <div className="relative" key={`myProjects@${i}`}>
             <video className="object-cover w-full h-full rounded-xl shadow-xl" autoPlay loop muted playsInline>
               <source src={p.preview} type="video/webm" />
               {p.previewMp4 && <source src={p.previewMp4} type="video/mp4" />}
               Your browser does not support the <code>video</code> element.
             </video>
-          </a>
+            <div className="text-6xl absolute flex top-0 left-0 w-full h-full opacity-0 rounded-xl bg-black items-center justify-center md:text-3xl hover:opacity-100">
+              {p.url && (
+                <a
+                  className="m-3 text-white rounded-lg md:m-2 md:rounded focus:outline-none"
+                  href={p.url}
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <MdOpenInNew />
+                </a>
+              )}
+              {p.sourceCode && (
+                <a
+                  className="m-3 text-white rounded-lg md:m-2 md:rounded focus:outline-none"
+                  href={p.sourceCode}
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <BsCodeSlash />
+                </a>
+              )}
+            </div>
+          </div>
         ))}
       </div>
     </>

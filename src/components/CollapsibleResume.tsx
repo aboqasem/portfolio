221bbaebaa11d23dafd230e4a95afadd13dea6af
Mohammad Al Zouabi <62098043+aboqasem@users.noreply.github.com>
2021-03-05 17:03:40 +0800

    Animate resume and projects

--- src/components/CollapsibleResume.tsx
+++ src/components/CollapsibleResume.tsx
@@ -8,22 +8,25 @@ const CollapsibleResume = (): JSX.Element => {
 
   const toggleResume = () => {
     setIsResumeShown((state) => !state);
-    resumeRef.current?.classList.toggle('hidden');
   };
 
   return (
     <div className="flex flex-col w-full">
-      <div className="relative w-full h-full hidden" style={{ paddingTop: '75%' }} ref={resumeRef}>
+      <div
+        className="relative w-full h-full transition-spacing"
+        style={{ paddingTop: isResumeShown ? '75%' : '0' }}
+        ref={resumeRef}
+      >
         <iframe className="w-full h-full absolute left-0 top-0" src={ResumePdf}></iframe>
       </div>
 
       <div
         className="w-full flex flex-col items-center text-2xl rounded-b-lg pt-3 cursor-pointer sm:text-3xl md:text-4xl"
-        title={`${!isResumeShown ? 'Show Resume' : 'Hide Resume'}`}
+        title={`${isResumeShown ? 'Hide Resume' : 'Show Resume'}`}
         style={{ backgroundColor: 'rgba(0, 0, 0, 0.03)' }}
         onClick={toggleResume}
       >
-        {!isResumeShown ? <IoIosArrowDown className="animate-bounce" /> : <IoIosArrowUp className="animate-bounce" />}
+        {isResumeShown ? <IoIosArrowUp className="animate-bounce" /> : <IoIosArrowDown className="animate-bounce" />}
       </div>
     </div>
   );

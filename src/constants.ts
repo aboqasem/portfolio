e2eb31bb96b5b1c0ba5f205790b39e26de3b74da
Mohammad Al Zouabi <mb.alzouabi@gmail.com>
2021-03-21 02:50:19 +0800

    Prefix constants with `k`

--- src/constants.ts
+++ src/constants.ts
@@ -2,4 +2,4 @@
 
 export const __DEV__ = process.env.NODE_ENV === 'development';
 
-export const apiUrl = process.env.REACT_APP_API_URL || 'https://api-aboqasem.herokuapp.com/';
+export const kApiUrl = process.env.REACT_APP_API_URL || 'https://api-aboqasem.herokuapp.com/';

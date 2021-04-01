b8919bec19fd54053940eede22e522744c245c51
Mohammad Al Zouabi <mb.alzouabi@gmail.com>
2021-04-01 19:47:13 +0800

    Initial production ready next.js project

--- old_ui/src/store/index.ts
+++
@@ -1,4 +0,0 @@
-export * from './types';
-export * from './selectors';
-export * from './connectors';
-export { default } from './store';
--- old_ui/src/store/rainDrops/actions.ts
+++
@@ -1,9 +0,0 @@
-import { RainDrops } from '../../components/Rain';
-import { RainDropsDispatchTypes, RAIN_DROPS_INIT } from './actionTypes';
-
-export const initializeRainDrops = (rainDrops: RainDrops): RainDropsDispatchTypes => {
-  return {
-    type: RAIN_DROPS_INIT,
-    payload: rainDrops,
-  };
-};

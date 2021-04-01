3fa833188fe18bba7c8d724403bc7e268c15b2d9
Mohammad Al Zouabi <62098043+aboqasem@users.noreply.github.com>
2021-04-01 08:33:35 +0800

    Add public manifest file

--- next/pages/_app.tsx
+++ next/pages/_app.tsx
@@ -1,8 +1,23 @@
 import { AppProps } from 'next/app';
+import Head from 'next/head';
 import '@/styles/globals.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <meta charSet="utf-8" />
+        <meta content="width=device-width, initial-scale=1" name="viewport" />
+        <title>Mohammad Al Zouabi</title>
+        <link rel="apple-touch-icon" sizes="180x180" href="/icons/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/icons/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/icons/favicon-16x16.png" />
+        <link href="/manifest.json" rel="manifest" />
+        <meta content="Mohammad Al Zouabi's personal website." name="description" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;

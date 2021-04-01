446d0a52bd6f1ddf5452d0df46101d26b7961655
Mohammad Al Zouabi <62098043+aboqasem@users.noreply.github.com>
2021-04-01 18:18:04 +0800

    Add Blog

---
+++ next/components/AvatarLink.tsx
@@ -0,0 +1,23 @@
+interface IProps {
+  lg?: boolean;
+}
+
+const AvatarLink = ({ lg }: IProps): JSX.Element => {
+  return (
+    <a
+      className="flex items-center text-black hover:underline"
+      href="https://github.com/aboqasem"
+      target="_blank"
+      rel="noreferrer"
+    >
+      <img
+        alt="Me"
+        className={`block rounded-full ${lg ? 'h-10 md:h-12' : 'h-8 md:h-10'}`}
+        src="https://github.com/aboqasem.png"
+      />
+      <p className={`ml-2 ${lg ? 'text-base md:text-xl' : 'text-sm md:text-lg'}`}>aboqasem</p>
+    </a>
+  );
+};
+
+export default AvatarLink;
---
+++ next/pages/blog/[id].tsx
@@ -0,0 +1,60 @@
+import { useRouter } from 'next/dist/client/router';
+import Head from 'next/head';
+import React from 'react';
+import ReactMarkdown from 'react-markdown';
+import { useSelector } from 'react-redux';
+
+import AvatarLink from '@/components/AvatarLink';
+import Center from '@/components/Center';
+import Loading from '@/components/Loading';
+import { RootState } from '@/store';
+
+const BlogPost = () => {
+  const id = useRouter().query.id as string;
+
+  const { areLoading, blogPosts } = useSelector((state: RootState) => state.blogPosts);
+  const post = blogPosts.get(id);
+
+  if (areLoading) return <Loading />;
+
+  if (!post)
+    return (
+      <Center>
+        <p className="text-3xl text-center text-red-800 sm:text-5xl md:text-6xl">Post Not Found</p>
+      </Center>
+    );
+
+  const { title, content, img, createdAt } = post;
+
+  return (
+    <>
+      <Head>
+        <title>{title}</title>
+        <meta name="description" content={content} />
+      </Head>
+
+      <div className="pb-20">
+        <img
+          className="object-cover w-screen h-64"
+          src={img || `https://dummyimage.com/700x200/000000/E1DFDC&text=${title}`}
+          alt={title}
+        />
+        <div className="flex items-center justify-between px-10 py-4">
+          <AvatarLink lg />
+          <p
+            className="text-base text-gray-700 md:text-lg"
+            title={createdAt.toString()}
+          >{`${createdAt.toDateString()}`}</p>
+        </div>
+        <hr className="pb-4 border-gray-400 sm:pb-6 md:pb-8" />
+        <Center>
+          <article className="prose prose-lg sm:prose-xl">
+            <ReactMarkdown>{`# ${title}\n${content}`}</ReactMarkdown>
+          </article>
+        </Center>
+      </div>
+    </>
+  );
+};
+
+export default BlogPost;

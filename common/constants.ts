edcd9aad211d0c2df8a9fbe97bb0cf4a7354cd4b
Mohammad Al Zouabi <62098043+aboqasem@users.noreply.github.com>
2021-04-01 21:59:33 +0800

    Fix API URL

--- common/constants.ts
+++ common/constants.ts
@@ -1,3 +1,4 @@
 export const __PROD__ = process.env.NODE_ENV === 'production';
 
-export const kApiUrl = process.env.NEXT_PUBLIC_API_URL;
+export const kApiUrl =
+  process.env.NEXT_PUBLIC_API_URL ?? (__PROD__ ? 'https://api-aboqasem.herokuapp.com/' : 'http://localhost:8000/');

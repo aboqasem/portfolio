8e3c39a43395fd82f2f20f89549a12722767da99
Mohammad Al Zouabi <mb.alzouabi@gmail.com>
2021-04-01 16:00:15 +0800

    Add About

--- next/components/index.ts
+++ next/components/index.ts
@@ -3,3 +3,4 @@ export { default as Navbar } from './Navbar';
 export { default as NavLink } from './NavLink';
 export { default as CollapsibleResume } from './CollapsibleResume';
 export { default as MyCard } from './MyCard';
+export { default as Project } from './Project';

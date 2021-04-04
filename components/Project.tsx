996b5d51cf85321934e68deda059b242f5745a18
Mohammad Al Zouabi <mb.alzouabi@gmail.com>
2021-04-04 14:15:15 +0800

    Add project description

--- components/Project.tsx
+++ components/Project.tsx
@@ -7,7 +7,7 @@ interface IProps {
   project: IProject;
 }
 
-const Project = ({ project: { preview, previewMp4, url, sourceCode } }: IProps) => {
+const Project = ({ project: { title, description, preview, previewMp4, url, sourceCode } }: IProps) => {
   return (
     <div className="relative">
       <video className="object-cover w-full h-full shadow-xl rounded-xl" autoPlay loop muted playsInline>
@@ -15,18 +15,34 @@ const Project = ({ project: { preview, previewMp4, url, sourceCode } }: IProps) 
         {previewMp4 && <source src={previewMp4} type="video/mp4" />}
         Your browser does not support the <code>video</code> element.
       </video>
-      <div className="absolute top-0 left-0 flex items-center justify-center w-full h-full text-6xl transition-opacity bg-black opacity-0 rounded-xl md:text-3xl hover:opacity-100">
-        {url && (
-          <a className="m-3 text-white rounded-lg md:m-2 md:rounded" href={url} target="_blank" rel="noreferrer">
-            <MdOpenInNew />
-          </a>
-        )}
-        {sourceCode && (
-          <a className="m-3 text-white rounded-lg md:m-2 md:rounded" href={sourceCode} target="_blank" rel="noreferrer">
-            <BsCodeSlash />
-          </a>
-        )}
-      </div>
+      <button className="absolute top-0 left-0 flex flex-col items-center justify-center w-full h-full text-white transition-opacity bg-black opacity-0 focus:outline-none rounded-xl focus:opacity-100">
+        <div className="grid grid-flow-row p-4 text-2xl text-center md:text-lg">
+          <p className="font-bold">{title}</p>
+          <p>{description}</p>
+        </div>
+        <div className="grid grid-flow-col">
+          {url && (
+            <a
+              className="m-3 text-6xl rounded-lg md:text-3xl md:m-2 md:rounded"
+              href={url}
+              target="_blank"
+              rel="noreferrer"
+            >
+              <MdOpenInNew />
+            </a>
+          )}
+          {sourceCode && (
+            <a
+              className="m-3 text-6xl rounded-lg md:text-3xl md:m-2 md:rounded"
+              href={sourceCode}
+              target="_blank"
+              rel="noreferrer"
+            >
+              <BsCodeSlash />
+            </a>
+          )}
+        </div>
+      </button>
     </div>
   );
 };

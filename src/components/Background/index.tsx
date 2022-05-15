1930cd089b7c0fe9f899b5b066f9da9ba03e3b61
Mohammad Al Zouabi <mb.alzouabi@gmail.com>
2022-05-15 20:39:21 +0800

    feat(background): add `Stage` and display tech icons

--- src/components/Background/index.tsx
+++ src/components/Background/index.tsx
@@ -1,5 +1,10 @@
+import { Stage } from '@/components/Stage';
 import { memo } from '@/utils/react/memo';
 
 export const Background = memo(function Background() {
-  return <div className="fixed inset-0 w-screen h-screen bg-[#faf5ef] z-0"></div>;
+  return (
+    <div className="fixed inset-0 w-screen h-screen bg-[#faf5ef] z-0">
+      <Stage />
+    </div>
+  );
 });

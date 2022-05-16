186b2a88581954fc9a7b62d8b86606b017c51499
Mohammad Al Zouabi <62098043+aboqasem@users.noreply.github.com>
2022-05-16 09:25:57 +0800

    feat(background/icons): add `desc` to icons and use as elements' `title`

--- src/components/Stage/index.tsx
+++ src/components/Stage/index.tsx
@@ -9,9 +9,10 @@ export const Stage = memo(function Stage() {
 
   return (
     <div ref={stageRef} className="relative inset-0 w-full h-full">
-      {icons.map((Icon, i) => (
-        <Icon
+      {icons.map((icon, i) => (
+        <icon.Icon
           key={i}
+          title={icon.desc}
           className="absolute text-2xl sm:text-3xl md:text-4xl"
           style={{
             top: positions[i]!.top,

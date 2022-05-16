3017fd5407e501a70eb8da2e31b7d4854db0a5c3
Mohammad Al Zouabi <62098043+aboqasem@users.noreply.github.com>
2022-05-16 09:25:57 +0800

    feat(background/icons): add `desc` to icons and use as elements' `title`

--- src/components/Stage/icons.ts
+++ src/components/Stage/icons.ts
@@ -38,42 +38,150 @@ import {
 } from 'react-icons/si';
 
 export const icons = [
-  SiTypescript,
-  SiJavascript,
-  SiDart,
-  SiJava,
-  SiCplusplus,
-  SiC,
-  SiGnubash,
-  GoMarkGithub,
-  MdHttp,
-  SiNodedotjs,
-  SiExpress,
-  SiReact,
-  SiNextdotjs,
-  SiNestjs,
-  SiJson,
-  SiFlutter,
-  SiMongodb,
-  SiPostgresql,
-  SiRedux,
-  SiHtml5,
-  SiCss3,
-  SiTailwindcss,
-  SiRaspberrypi,
-  SiArduino,
-  SiHomebrew,
-  SiDocker,
-  SiIterm2,
-  SiInsomnia,
-  SiPostman,
-  SiIntellijidea,
-  SiVisualstudiocode,
-  SiMarkdown,
-  SiNpm,
-  SiPnpm,
-  SiYarn,
-  SiMacos,
+  {
+    Icon: SiTypescript,
+    desc: 'TypeScript',
+  },
+  {
+    Icon: SiJavascript,
+    desc: 'JavaScript',
+  },
+  {
+    Icon: SiDart,
+    desc: 'Dart',
+  },
+  {
+    Icon: SiJava,
+    desc: 'Java',
+  },
+  {
+    Icon: SiCplusplus,
+    desc: 'C++',
+  },
+  {
+    Icon: SiC,
+    desc: 'C',
+  },
+  {
+    Icon: SiGnubash,
+    desc: 'GNU Bash',
+  },
+  {
+    Icon: GoMarkGithub,
+    desc: 'Github',
+  },
+  {
+    Icon: MdHttp,
+    desc: 'HTTP',
+  },
+  {
+    Icon: SiNodedotjs,
+    desc: 'Node.js',
+  },
+  {
+    Icon: SiExpress,
+    desc: 'Express.js',
+  },
+  {
+    Icon: SiReact,
+    desc: 'React.js',
+  },
+  {
+    Icon: SiNextdotjs,
+    desc: 'Next.js',
+  },
+  {
+    Icon: SiNestjs,
+    desc: 'Nest.js',
+  },
+  {
+    Icon: SiJson,
+    desc: 'JSON',
+  },
+  {
+    Icon: SiFlutter,
+    desc: 'Flutter',
+  },
+  {
+    Icon: SiMongodb,
+    desc: 'MongoDB',
+  },
+  {
+    Icon: SiPostgresql,
+    desc: 'PostgreSQL',
+  },
+  {
+    Icon: SiRedux,
+    desc: 'Redux',
+  },
+  {
+    Icon: SiHtml5,
+    desc: 'HTML5',
+  },
+  {
+    Icon: SiCss3,
+    desc: 'CSS3',
+  },
+  {
+    Icon: SiTailwindcss,
+    desc: 'Tailwind CSS',
+  },
+  {
+    Icon: SiRaspberrypi,
+    desc: 'Raspberry Pi',
+  },
+  {
+    Icon: SiArduino,
+    desc: 'Arduino',
+  },
+  {
+    Icon: SiHomebrew,
+    desc: 'Homebrew',
+  },
+  {
+    Icon: SiDocker,
+    desc: 'Docker',
+  },
+  {
+    Icon: SiIterm2,
+    desc: 'iTerm 2',
+  },
+  {
+    Icon: SiInsomnia,
+    desc: 'Insomnia',
+  },
+  {
+    Icon: SiPostman,
+    desc: 'Postman',
+  },
+  {
+    Icon: SiIntellijidea,
+    desc: 'IntelliJ Idea',
+  },
+  {
+    Icon: SiVisualstudiocode,
+    desc: 'Visual Studio Code',
+  },
+  {
+    Icon: SiMarkdown,
+    desc: 'Markdown',
+  },
+  {
+    Icon: SiNpm,
+    desc: 'NPM',
+  },
+  {
+    Icon: SiPnpm,
+    desc: 'PNPM',
+  },
+  {
+    Icon: SiYarn,
+    desc: 'Yarn',
+  },
+  {
+    Icon: SiMacos,
+    desc: 'macOS',
+  },
 ];
 
 export const ICONS_LENGTH = icons.length;

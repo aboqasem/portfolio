66f88dfb50e224f3f660cd9c7925cb9e93119717
Mohammad Al Zouabi <mb.alzouabi@gmail.com>
2022-05-21 13:06:47 +0800

    feat(background): toggle stage settings

--- src/components/Stage/Settings.tsx
+++ src/components/Stage/Settings.tsx
@@ -2,6 +2,7 @@ import { RangeSlider } from '@/components/forms/elements/RangeSlider';
 import { IS_BROWSER } from '@/constants/shared';
 import { memo } from '@/utils/react/memo';
 import { useEffect, useState } from 'react';
+import { FiSettings } from 'react-icons/fi';
 
 export const stageSettings = {
   dropSpeed: -0,
@@ -10,6 +11,8 @@ export const stageSettings = {
 export interface StageSettingsProps {}
 
 export const StageSettings = memo(function StageSettings(_: StageSettingsProps) {
+  const [show, setShow] = useState(true);
+
   const [dropSpeed, setDropSpeed] = useState(() => {
     const allowMotion = IS_BROWSER && !window.matchMedia('(prefers-reduced-motion)').matches;
 
@@ -21,18 +24,31 @@ export const StageSettings = memo(function StageSettings(_: StageSettingsProps) 
   }, [dropSpeed]);
 
   return (
-    <div className="absolute top-0 right-0 p-2 mt-2 bg-white shadow-md rounded-tl-xl rounded-bl-xl">
-      <RangeSlider
-        id="stage-drop-speed"
-        label="Speed"
-        min={0}
-        max={3}
-        step={0.2}
-        value={dropSpeed}
-        onChange={({ target: { valueAsNumber } }) => {
-          setDropSpeed(valueAsNumber);
-        }}
-      />
+    <div className="absolute top-0 right-0 flex items-start mt-2">
+      <button
+        type="button"
+        className="p-2 bg-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-dallas rounded-l-xl"
+        onClick={() => setShow((show) => !show)}
+      >
+        <FiSettings className="text-lg" />
+        <span className="sr-only">Stage Settings</span>
+      </button>
+
+      {show && (
+        <div className="p-2 bg-white rounded-bl-xl">
+          <RangeSlider
+            id="stage-drop-speed"
+            label="Drop speed"
+            min={0}
+            max={3}
+            step={0.2}
+            value={dropSpeed}
+            onChange={({ target: { valueAsNumber } }) => {
+              setDropSpeed(valueAsNumber);
+            }}
+          />
+        </div>
+      )}
     </div>
   );
 });

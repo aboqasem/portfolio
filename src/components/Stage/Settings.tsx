73d68a1f1dd015b9bc3eb1882a5118a1eb43d7a3
Mohammad Al Zouabi <62098043+aboqasem@users.noreply.github.com>
2022-05-24 16:34:30 +0800

    feat(bg/stage): animate stage settings

--- src/components/Stage/Settings.tsx
+++ src/components/Stage/Settings.tsx
@@ -11,7 +11,7 @@ export const stageSettings = {
 export interface StageSettingsProps {}
 
 export const StageSettings = memo(function StageSettings(_: StageSettingsProps) {
-  const [show, setShow] = useState(false);
+  const [isShown, setIsShown] = useState(false);
 
   const [dropSpeed, setDropSpeed] = useState(() => {
     const storedDropSpeed = +(localStorage.getItem('stageSettings.dropSpeed') ?? NaN);
@@ -32,31 +32,34 @@ export const StageSettings = memo(function StageSettings(_: StageSettingsProps) 
   }, [dropSpeed]);
 
   return (
-    <div className="absolute top-0 right-0 flex items-start mt-2">
+    <div
+      className={`absolute top-0 transition-[right] motion-reduce:!transition-none flex items-start mt-2 ${
+        isShown ? 'right-0' : `-right-[10rem]`
+      }`}
+    >
       <button
         type="button"
         className="p-2 border border-r-0 bg-stone-50 group group-hover:bg-stone-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-linen-800 rounded-l-xl"
-        onClick={() => setShow((show) => !show)}
+        onClick={() => setIsShown((show) => !show)}
       >
         <FiSettings className="text-lg group group-hover:animate-[spin_2s_linear_infinite] group-focus:animate-[spin_2s_linear_infinite] motion-reduce:!animate-none" />
-        <span className="sr-only">Stage Settings</span>
+        <span className="sr-only">{isShown ? 'Hide' : 'Show'} stage settings</span>
       </button>
 
-      {show && (
-        <div className="p-2 border bg-stone-50 rounded-bl-xl">
-          <RangeSlider
-            id="stage-drop-speed"
-            label="Drop speed"
-            min={0}
-            max={3}
-            step={0.2}
-            value={dropSpeed}
-            onChange={({ target: { valueAsNumber } }) => {
-              setDropSpeed(valueAsNumber);
-            }}
-          />
-        </div>
-      )}
+      <div className={`w-[10rem] p-2 border bg-stone-50 rounded-bl-xl`}>
+        <RangeSlider
+          id="stage-drop-speed"
+          label="Drop speed"
+          min={0}
+          max={3}
+          step={0.2}
+          value={dropSpeed}
+          disabled={!isShown}
+          onChange={({ target: { valueAsNumber } }) => {
+            setDropSpeed(valueAsNumber);
+          }}
+        />
+      </div>
     </div>
   );
 });

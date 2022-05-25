77c337b0119d7f280afa34f87f40d33e950d10a7
Mohammad Al Zouabi <62098043+aboqasem@users.noreply.github.com>
2022-05-25 17:34:00 +0800

    chore: remove `old` directory

--- old/store/rainDrops/index.ts
+++
@@ -1,4 +0,0 @@
-export * from './types';
-export * from './actionTypes';
-export * from './actions';
-export { default } from './reducer';
--- old/store/selectors.ts
+++
@@ -1,7 +0,0 @@
-import { ICommonDataState } from './commonData';
-import { IRainDropsState } from './rainDrops';
-import { RootState } from './types';
-
-export const selectRainDropsState = (state: RootState): IRainDropsState => state.rainDrops;
-
-export const selectCommonDataState = (state: RootState): ICommonDataState => state.commonData;

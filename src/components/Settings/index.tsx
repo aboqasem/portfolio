5b7d8d7183c8c424d247f2cc41a53df6f2537ddd
Mohammad Al Zouabi <mb.alzouabi@gmail.com>
2022-06-06 21:24:08 +0800

    feat(settings): add tooltip

--- src/components/Settings/index.tsx
+++ src/components/Settings/index.tsx
@@ -6,6 +6,8 @@ import { FiSettings } from 'react-icons/fi';
 export const Settings = memo(function Settings() {
   const [isShown, setIsShown] = useState(false);
 
+  const hint = `${isShown ? 'Hide' : 'Show'} settings`;
+
   return (
     <div
       className={`absolute top-0 transition-[right] motion-reduce:!transition-none flex items-start mt-2 ${
@@ -14,11 +16,12 @@ export const Settings = memo(function Settings() {
     >
       <button
         type="button"
+        title={hint}
         className="p-2 border border-r-0 bg-stone-50 group group-hover:bg-stone-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-linen-800 rounded-l-xl"
         onClick={() => setIsShown((show) => !show)}
       >
         <FiSettings className="text-lg group group-hover:animate-[spin_2s_linear_infinite] group-focus:animate-[spin_2s_linear_infinite] motion-reduce:!animate-none" />
-        <span className="sr-only">{isShown ? 'Hide' : 'Show'} settings</span>
+        <span className="sr-only">{hint}</span>
       </button>
 
       <div className={`w-[10rem] border bg-stone-50 rounded-bl-xl divide-y`}>

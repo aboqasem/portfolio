537e2c3ad53ad9ab96a92eeb3e8d0b781c84a2a6
Mohammad Al Zouabi <mb.alzouabi@gmail.com>
2022-06-20 09:33:29 +0800

    perf(configure-theme): remove extra logic

--- public/js/theme/configure-theme.js
+++ public/js/theme/configure-theme.js
@@ -1,19 +1,6 @@
 (() => {
-  let theme;
-  const storedTheme = localStorage.getItem('theme');
-  if (storedTheme && (storedTheme === 'Dark' || storedTheme === 'Light')) {
-    theme = storedTheme;
-  }
-
-  if (!theme) {
-    localStorage.removeItem('theme');
-  } else {
-    localStorage.setItem('theme', theme);
-  }
-
-  if (theme === 'Dark' || (!theme && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
-    document.documentElement.classList.add('dark');
-  } else {
-    document.documentElement.classList.remove('dark');
-  }
+  const { theme } = localStorage;
+  document.documentElement.classList[
+    theme === 'Dark' || (theme !== 'Light' && window.matchMedia('(prefers-color-scheme: dark)').matches) ? 'add' : 'remove'
+  ]('dark');
 })();

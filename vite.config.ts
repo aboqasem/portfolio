868293563974bc7e7868802474bfbbbd08782048
Mohammad Al Zouabi <62098043+aboqasem@users.noreply.github.com>
2022-06-24 18:52:04 +0800

    perf: minify `index.html`

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,16 @@
 import { defineConfig } from 'vite';
 import eslintPlugin from 'vite-plugin-eslint';
+import minifyHtmlPlugin from 'vite-plugin-minify';
 import solidPlugin from 'vite-plugin-solid';
 import tsconfigPathsPlugin from 'vite-tsconfig-paths';
 
 export default defineConfig({
-  plugins: [eslintPlugin(), tsconfigPathsPlugin(), solidPlugin()],
+  plugins: [
+    eslintPlugin(),
+    tsconfigPathsPlugin(),
+    solidPlugin(),
+    minifyHtmlPlugin({ minifyJS: true }),
+  ],
   build: {
     target: 'esnext',
     polyfillDynamicImport: false,

6ba77dc0bfd15f3927bfe6cebc13b1cce042aede
Mohammad Al Zouabi <mb.alzouabi@gmail.com>
2025-02-13 21:15:45 +0800

    feat(analytics): use piwik pro

--- src/index.tsx
+++ src/index.tsx
@@ -3,4 +3,12 @@ import { App } from "@/App";
 import "@/styles/globals.css";
 import { render } from "solid-js/web";
 
+if (import.meta.env.PROD) {
+	import("@piwikpro/tracking-base-library")
+		.then(({ default: PiwikPro }) => {
+			PiwikPro.initialize(import.meta.env.VITE_PIWIK_CONTAINER_ID, import.meta.env.VITE_PIWIK_CONTAINER_URL);
+		})
+		.catch(() => {});
+}
+
 render(() => <App />, document.getElementById("root")!);

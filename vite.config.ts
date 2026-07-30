941f8d292307d1d6ad3aa25112e5c651c05c5cd7
Mohammad Al Zouabi <mb.alzouabi@gmail.com>
2026-07-30 11:42:52 +0200

    chore(deps): update vite

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,13 @@ import tailwindcss from "@tailwindcss/vite";
 import { defineConfig, type Plugin } from "vite";
 import { ViteMinifyPlugin } from "vite-plugin-minify";
 import solidPlugin from "vite-plugin-solid";
-import tsconfigPathsPlugin from "vite-tsconfig-paths";
 import mainCssBeforeMainJs from "./plugins/vite/main-css-before-main-js";
 
 export default defineConfig({
+	resolve: {
+		tsconfigPaths: true,
+	},
 	plugins: [
-		tsconfigPathsPlugin(),
 		solidPlugin() as unknown as Plugin,
 		tailwindcss(),
 		ViteMinifyPlugin({ minifyJS: true }) as unknown as Plugin,
